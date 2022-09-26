26275538f7483403e40411a567d08dfc41beb378
Marnixdo <113102273+Marnixdo@users.noreply.github.com>
2022-09-26 13:34:27 +0200

    Add files via upload

---
+++ leren programmeren/module_2/Solicitatie.py
@@ -0,0 +1,23 @@
+snor = 0
+haar = 0
+
+diploma = input("Ben je in bezit van een ondernemen MBO 4 diploma? ")
+rijbewijs = input("Ben je in bezit van een geldig vrachtwagen rijbewijs? ")
+hoed = input("Ben je in bezit van een hoge hoed? ")
+manvrouw = input("Bent u een man of een vrouw m/v? ")
+if manvrouw == "m":
+    snor = int(input("Hoe lang is uw snor? "))
+if manvrouw == "v":
+    haar = int(input("hoe lang is uw haar? "))
+lengte = input("hoe lang bent u? in cm ")
+gewicht = input("hoe zwaar bent u? in kilo's ")
+certificaat = input("heeft u een certificaat van Overleven met gevaarlijk personeel? ")
+ervaring_1 = int(input("hoeveel jaar ervaring heeft u met dieren-dressuur? "))
+ervaring_2 = int(input("hoeveel jaar ervaring heeft u met jongleren? "))
+ervaring_3 = int(input("hoeveel jaar heeft u ervaring met acrobatiek? "))
+
+
+if diploma == ("ja") and rijbewijs == ("ja") and hoed == ("ja") and (manvrouw == "man" and snor >10) or (manvrouw == "vrouw" and haar >20) and lengte >150 and lengte <220 and gewicht >90 and gewicht <120 and certificaat == ("ja") and (ervaring_1 >4 or ervaring_2 >5 or ervaring_3 >3):
+    print("gefeliciteerd, u heeft uw solicitatie fantastisch afgeleverd! ")
+else: 
+    print("u heeft uw solicitatie helaas niet goed genoeg ingevuld om aangenomen te worden. ")
