70e6296a23c3963a1d786f61c1d258f1603fea34
Marnixdo <113102273+Marnixdo@users.noreply.github.com>
2022-11-04 09:54:34 +0100

    Delete leren programmeren directory

--- leren programmeren/module_3/blastoff.py
+++
@@ -1,7 +0,0 @@
-import time
-
-for c in range(30,0,-1):
-    print (c)
-    time.sleep(1)
-print("BLAST OFF")
-
--- leren programmeren/module_3/detijd.py
+++
@@ -1,6 +0,0 @@
-for x in range(1, 25):
-    if x < 13:
-        print(f"{x} AM")
-    else:
-        print(f"{x-12} PM")
-
